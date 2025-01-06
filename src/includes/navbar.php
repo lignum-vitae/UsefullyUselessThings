274f0c6ce95162eadec2a00017d0fa7793497729
Andrew Hennis <dawnandrew100@gmail.com>
2025-01-05 21:44:10 -0500

    navbar works on edge works!

--- src/includes/navbar.php
+++ src/includes/navbar.php
@@ -1,6 +1,4 @@
-<a href="index.html" target="_parent">Home</a>
+<a href="index.php" target="_parent">Home</a>
 <a href="playground.html" target="_parent">Playground</a>
 <a href="#contact" target="_parent">Contact</a>
 <a href="about.html" target="_parent">About</a>
-
-<script type="text/javascript" src="scripts/navbar.js"></script>
--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -2,18 +2,18 @@ let navbar = document.getElementById("navbar");
 
 let navPos = navbar.getBoundingClientRect().top;
 
-window.addEventListener("scroll", e => {
+window.addEventListener("scroll", (e) => {
   let scrollPos = window.scrollY;
   if (scrollPos > navPos) {
-    navbar.classList.add('transition');
-    navbar.classList.remove('endtransition');
-    header.classList.add('navbarOffsetMargin');
+    navbar.classList.add("transition");
+    navbar.classList.remove("endtransition");
+    header.classList.add("navbarOffsetMargin");
   } else {
-    navbar.classList.add('endtransition');
-    header.classList.remove('navbarOffsetMargin');
+    navbar.classList.add("endtransition");
+    header.classList.remove("navbarOffsetMargin");
   }
 });
 
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
-}
+};
