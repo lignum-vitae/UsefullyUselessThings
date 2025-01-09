e5507af563f534f8590f9d47aa71ae65b7a2329b
Andrew Hennis <dawnandrew100@gmail.com>
2025-01-09 18:52:56 -0500

    close active buffers

--- src/index.php
+++ src/index.php
@@ -3,5 +3,6 @@ $title = "Welcome!";
 ob_start();
 include "nav/index.html";
 $content = ob_get_clean();
+ob_end_clean();
 ?>
 <?php include "includes/landing.php" ?>
--- src/playground.php
+++ src/playground.php
@@ -3,6 +3,7 @@ $title = "";
 ob_start();
 include "nav/playground.html";
 $content = ob_get_clean();
+ob_end_clean();
 ?>
 
 <?php include "includes/landing.php" ?>
